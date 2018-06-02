70b6f7d01c8968f75144394a3bc0bab089dbd524
Edier Bernal Rozo <edier.bernal@outlook.com>
2018-06-02 12:21:27 -0500

    Configuración y actualización de librerias

--- SystemExpert/Controllers/MessagesController.cs
+++ SystemExpert/Controllers/MessagesController.cs
@@ -18,7 +18,7 @@ namespace SystemExpert
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                await Conversation.SendAsync(activity, () => new Dialogs.QnADialog());
             }
             else
             {
---
+++ SystemExpert/Dialogs/QnADialog.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SystemExpert.Dialogs
+{
+    [Serializable]
+    public class QnADialog: QnAMakerDialog
+    {
+        public QnADialog (): base(new QnAMakerService (new QnAMakerAttribute(ConfigurationManager.AppSettings["QnASuscriptionKey"], ConfigurationManager.AppSettings["QnAKnowledgebaseId"], "No encontre su respuesta", 0,5)))
+        {
+
+        }
+    }
+}
